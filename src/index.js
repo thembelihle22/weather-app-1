8fe1378f296215b8b9f7b08d9952a2e778c5b404
thembelihle22 <thembelihlegasa22@gmail.com>
2024-08-23 09:29:52 +0200

    JavaScript file created, with basic js.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-function citySearch(event) {
+function citySearchForm(event) {
   event.preventDefault();
+  let searchInput = document.querySelector("#search");
+  let cityElement = document.querySelector("#h1");
+  cityElement = searchInput.value;
 }
-
 let search = document.querySelector("#form-input");
-search.addEventListener("sumbit", citySearch);
+search.addEventListener("sumbit", citySearchForm);
