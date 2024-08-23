a734236f40d826ab0b2f32eaa8f8fe0c5e056ebd
thembelihle22 <thembelihlegasa22@gmail.com>
2024-08-23 18:18:05 +0200

    added axios

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ function citySearchForm(event) {
   let searchInput = document.querySelector("#search");
   let cityElement = document.querySelector("#h1");
   cityElement.innerHTML = searchInput.value;
-  console.log(searchInput.value);
+
+  let apiKey = "59tfe2c834300cb76dbcd14b56oa0baf";
+  let api =
+    "https://api.shecodes.io/weather/v1/current?query=searchInput.value&key=apiKey&units=metric";
 }
 
 let search = document.querySelector("#form-input");
